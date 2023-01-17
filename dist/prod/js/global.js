7ba888ff1724ff1b7921ab22ad23f9fefa60c5b2
CarlosAlbertoM <carlos.msam@tecmilenio.mx>
2023-01-16 19:54:09 -0600

    Big update

---
+++ dist/prod/js/global.js
@@ -0,0 +1,219 @@
+"use strict";
+
+// Variables
+var bodyWith = document.getElementsByTagName('body')[0].clientWidth;
+
+// Ejecutar funciones
+window.addEventListener('load', function () {
+  // headerScripts();
+  // if ( bodyWith <= 568  ){
+
+  //    footerScripts();
+
+  // }
+});
+
+// Funciones
+var headerScripts = function headerScripts() {
+  // Variables
+
+  var headerMain = document.querySelector('header');
+  var mainNav = headerMain.querySelector('.mainNav--menu');
+  var nav__levelTwo = document.querySelectorAll('header .mainNav--menu ol.level--two');
+  var nav__levelThree = document.querySelectorAll('header .mainNav--menu ul.level--three');
+  var li__Global = mainNav.querySelectorAll('ul.level--one li');
+  var li__levelOne = mainNav.querySelectorAll('ul.level--one li a.actionDrop');
+  var li__levelTwo = mainNav.querySelectorAll('ol.level--two li');
+  var closeMenu = document.querySelector('.mobileActions--toggleMenu');
+  closeMenu.addEventListener('click', function () {
+    mainNav.classList.toggle('activeMenuMobile');
+  });
+
+  // Actions for desktop
+  if (bodyWith > 768) {
+    li__Global.forEach(function (m) {
+      m.addEventListener('mouseover', openDropsLevelTwo);
+    });
+    li__levelTwo.forEach(function (m) {
+      m.addEventListener('mouseover', openDropsLevelThree);
+    });
+  } else {
+    // For mobile
+    // Open drop level one
+    li__levelOne.forEach(function (m, index) {
+      m.addEventListener('click', function (event) {
+        openDropsLevelTwo(m, index);
+        event.stopPropagation();
+      });
+    });
+    // Open drop level two
+    li__levelTwo.forEach(function (m, index) {
+      m.addEventListener('click', function (event) {
+        m.classList.add('activeLinkDrop');
+        openDropsLevelThree(m, index);
+        event.stopPropagation();
+      });
+    });
+  }
+
+  // Accion para cerrar nav
+  // Descomentar al final
+  nav__levelTwo.forEach(function (m) {
+    m.addEventListener('mouseleave', closeNav);
+  });
+
+  // Funciones
+
+  // Interactuar con los enlaces
+  function openDropsLevelTwo(e, pos) {
+    // Desktop actions
+    if (bodyWith > 768) {
+      // Remover clase active a todos los ul
+      nav__levelTwo.forEach(function (n) {
+        if (n.classList.contains('activeMenu')) {
+          n.classList.remove('activeMenu');
+        }
+      });
+      // Si existe submenu agregar clase activa
+      if (this.querySelector('ol') != null) {
+        this.querySelector('ol').classList.add('activeMenu');
+      }
+    } else {
+      // Mobile actions
+      // Start for level two
+      // clean class
+      nav__levelTwo.forEach(function (m) {
+        if (m.classList.contains('activeMenu')) {
+          m.classList.remove('activeMenu');
+        }
+      });
+      // Add class
+      nav__levelTwo[pos].classList.add('activeMenu');
+      // Create back btn
+      var parr = document.createElement('div');
+      parr.classList.add('link-back-menu');
+      // Text brother elements
+      parr.textContent = nav__levelTwo[pos].previousElementSibling.textContent;
+      // Add div to ul
+      nav__levelTwo[pos].insertBefore(parr, nav__levelTwo[pos].firstChild);
+      // If exists action back btn
+      if (document.getElementsByClassName('link-back-menu').length > 0) {
+        document.getElementsByClassName('link-back-menu')[0].addEventListener('click', function (n) {
+          n.currentTarget.remove();
+          nav__levelTwo.forEach(function (m) {
+            if (m.classList.contains('activeMenu')) {
+              m.classList.remove('activeMenu');
+            }
+          });
+        });
+      }
+      ;
+      // End level two
+    }
+    // End
+  }
+  ;
+
+  // Open ul level three
+  function openDropsLevelThree(e, pos) {
+    // Desktop actions
+    if (bodyWith > 768) {
+      // Limpiar clase
+      document.querySelectorAll('.level--two--cta-link').forEach(function (m) {
+        if (m.classList.contains('visible-cta')) {
+          m.classList.remove('visible-cta');
+        }
+      });
+      nav__levelThree.forEach(function (m) {
+        if (m.classList.contains('activeMenu')) {
+          m.classList.remove('activeMenu');
+        }
+      });
+
+      // If cta exists
+      if (this.querySelector('.level--two--cta-link') != null) {
+        this.querySelector('.level--two--cta-link').classList.add('visible-cta');
+      }
+      // Si existe submenu agregar clase activa
+      if (this.querySelector('ul') != null) {
+        this.querySelector('ul').classList.add('activeMenu');
+      }
+    } else {
+      document.querySelectorAll('.level--three').forEach(function (m) {
+        if (m.classList.contains('activeDrop')) {
+          m.classList.remove('activeDrop');
+        }
+      });
+      if (e.querySelector('.level--three') != null) {
+        e.querySelector('.level--three').classList.toggle('activeDrop');
+      }
+    }
+    // End
+  }
+
+  // Close global menu
+  function closeNav() {
+    // Remover clase active a todos los ul
+    nav__levelTwo.forEach(function (n) {
+      if (n.classList.contains('activeMenu')) {
+        n.classList.remove('activeMenu');
+      }
+    });
+  }
+
+  // Funcion cuando se haga scroll 
+  window.addEventListener('scroll', function () {
+    if (window.scrollY < headerMain.clientHeight) {
+      if (headerMain.classList.contains('sticky-nav')) {
+        headerMain.classList.remove('sticky-nav');
+      }
+      headerMain.classList.add('down-effect');
+    } else {
+      if (headerMain.classList.contains('down-effect')) {
+        headerMain.classList.remove('down-effect');
+      }
+      headerMain.classList.add('sticky-nav');
+    }
+  });
+};
+
+// Funcion para accordeon sencillo
+var accordeonSection = function accordeonSection(selector) {
+  var accordeonBtn = Array.from(document.querySelector(selector + ' .accordeon-init').querySelectorAll('.accordeon-btn'));
+  accordeonBtn.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+      console.log(e);
+      for (var i = 0; i < document.querySelectorAll("".concat(selector, " .accordeon-btn")).length; i++) {
+        document.querySelectorAll("".concat(selector, " .accordeon-btn"))[i].parentNode.classList.contains('accordeon-active') && document.querySelectorAll("".concat(selector, " .accordeon-btn"))[i].parentNode.classList.remove('accordeon-active');
+      }
+
+      // Deshabilitar links en enlaces del head
+      e.currentTarget.querySelectorAll('a').forEach(function (m) {
+        m.addEventListener('click', function (e) {
+          return e.preventDefault();
+        });
+      });
+
+      // Toggle clase activa
+      // e.parentNode.classList.contains('accordeon-active') && e.parentNode.classList.remove('accordeon-active');
+
+      // Toggle clase activa
+      e.currentTarget.parentNode.classList.toggle('accordeon-active');
+      e.currentTarget.parentNode.parentNode.querySelector('.accordeon-box') != null && e.currentTarget.parentNode.parentNode.querySelector('.accordeon-box').classList.toggle('active');
+    });
+  });
+};
+// Funcion para accordeon sencillo
+var accordeonCareers = function accordeonCareers(selector) {
+  var accordeonBtn = Array.from(document.querySelector(selector + ' .accordeon-init').querySelectorAll('.accordeon-btn'));
+  accordeonBtn.forEach(function (btn) {
+    var index = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+    btn.addEventListener('click', function (e) {
+      for (var i = 0; i < document.querySelectorAll("".concat(selector, " ._c-careers-list-in-level--itm-accordeon")).length; i++) {
+        document.querySelectorAll("".concat(selector, " ._c-careers-list-in-level--itm-accordeon"))[i].classList.contains('accordeon-active') && document.querySelectorAll("".concat(selector, " ._c-careers-list-in-level--itm-accordeon"))[i].classList.remove('accordeon-active');
+      }
+      !document.querySelectorAll("".concat(selector, " ._c-careers-list-in-level--itm-accordeon"))[index].classList.contains('accordeon-active') && document.querySelectorAll("".concat(selector, " ._c-careers-list-in-level--itm-accordeon"))[index].classList.add('accordeon-active');
+      console.log(document.querySelectorAll("".concat(selector, " ._c-careers-list-in-level--itm-accordeon"))[index].classList.contains('accordeon-active'));
+    });
+  });
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const gulp = require('gulp');
-const {src, dest, series, watch, parallel} = require('gulp');
+const { src, dest, series, watch, parallel } = require('gulp');
 const fs = require('fs');
 const prefixer = require("gulp-autoprefixer");
 const sourcemaps = require('gulp-sourcemaps');
@@ -18,51 +18,76 @@ const prefixerOptions = {
 	overrideBrowserslist: ['last 3 versions']
 };
 // Html
-function htmlDesarrollo(done){
-		return src(configuracion.directorios.pug.fuente)
-			.pipe(pug({doctype: 'html', pretty: true}).on('error', error => {console.log('Error en pug: ' + error);}))
-			.pipe(dest(configuracion.directorios.pug.destino.dev))
-			.pipe(browserSync.reload({stream: true}));
+function htmlDesarrollo(done) {
+	return src(configuracion.directorios.pug.fuente)
+		.pipe(pug({ doctype: 'html', pretty: true }).on('error', error => { console.log('Error en pug: ' + error); }))
+		.pipe(dest(configuracion.directorios.pug.destino.dev))
+		.pipe(browserSync.reload({ stream: true }));
+}
+function htmlProd(done) {
+	return src(configuracion.directorios.pug.fuente)
+		.pipe(pug({ doctype: 'html', pretty: true }).on('error', error => { console.log('Error en pug: ' + error); }))
+		.pipe(dest(configuracion.directorios.pug.destino.prod))
+		.pipe(browserSync.reload({ stream: true }));
 }
 // Styles
 function compilarCssDesarrollo() {
-   return src('./sass/**/*.sass')
+	return src('./sass/**/*.sass')
 		.pipe(sourcemaps.init())
-      .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(sass.sync({ outputStyle: 'expanded' }).on('error', sass.logError))
 		.pipe(prefixer(prefixerOptions))
-      .pipe(sourcemaps.write('./maps'))
-      .pipe(dest(configuracion.directorios.sass.destino.dev))
+		.pipe(sourcemaps.write('./maps'))
+		.pipe(dest(configuracion.directorios.sass.destino.dev))
+		.pipe(browserSync.stream());
+}
+function compilarCssProd() {
+	return src('./sass/**/*.sass')
+		.pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
+		.pipe(prefixer(prefixerOptions))
+		.pipe(dest(configuracion.directorios.sass.destino.prod))
 		.pipe(browserSync.stream());
 }
 // Javascript
-function jsDesarrollo(){
+function jsDesarrollo() {
 	return src('./js/**/*.js')
 		.pipe(babel({
 			presets: ['@babel/env']
 		}))
-		.on('error', function(error){console.log(error.toString())})
+		.on('error', function (error) { console.log(error.toString()) })
 		.pipe(dest(configuracion.directorios.js.destino.dev))
 		.pipe(browserSync.stream());
 }
+function jsProd() {
+	return src('./js/**/*.js')
+		.pipe(babel({
+			presets: ['@babel/env']
+		}))
+		.on('error', function (error) { console.log(error.toString()) })
+		.pipe(dest(configuracion.directorios.js.destino.prod))
+		.pipe(browserSync.stream());
+}
 // Imagenes
-function imagenesDesarrollo(){
+function imagenesDesarrollo() {
 	return src(configuracion.directorios.imagenes.fuente)
 		.pipe(dest(configuracion.directorios.imagenes.destino.dev))
 }
+function imagenesProd() {
+	return src(configuracion.directorios.imagenes.fuente)
+		.pipe(dest(configuracion.directorios.imagenes.destino.prod))
+}
 // fonts
-function fontsDesarrollo(){
+function fontsDesarrollo() {
 	return src(configuracion.directorios.fonts.fuente)
 		.pipe(dest(configuracion.directorios.fonts.destino.dev))
 }
 // vendor all (slick, plugins...)
-function vendorAllDesarrollo(){
+function vendorAllDesarrollo() {
 	return src(configuracion.directorios.vendor.fuente)
 		.pipe(dest(configuracion.directorios.vendor.destino.dev))
 }
 
 // Browser sync
-function browserSyncDesarrollo(done){
-
+function browserSyncDesarrollo(done) {
 	browserSync.init({
 		injectChanges: true,
 		server: {
@@ -74,26 +99,60 @@ function browserSyncDesarrollo(done){
 		},
 		port: 3000
 	});
-	   
-   let watcherHtml = watch(configuracion.directorios.pug.watcher);
-   watcherHtml.on('change', function(path){htmlDesarrollo(path)});
-	
-	watch(configuracion.directorios.sass.watcher,     compilarCssDesarrollo);
-	watch("./js/**/*.js",                           jsDesarrollo);
-	watch(configuracion.directorios.imagenes.fuente,     imagenesDesarrollo);
-	watch(configuracion.directorios.fonts.fuente,     fontsDesarrollo);
-	watch(configuracion.directorios.vendor.fuente,     vendorAllDesarrollo);
-   watch(configuracion.directorios.pug.recargar.dev).on('change', browserSync.reload);
 
-   done();
+	let watcherHtml = watch(configuracion.directorios.pug.watcher);
+	watcherHtml.on('change', function (path) { htmlDesarrollo(path) });
+
+	watch(configuracion.directorios.sass.watcher, compilarCssDesarrollo);
+	watch("./js/**/*.js", jsDesarrollo);
+	watch(configuracion.directorios.imagenes.fuente, imagenesDesarrollo);
+	watch(configuracion.directorios.fonts.fuente, fontsDesarrollo);
+	watch(configuracion.directorios.vendor.fuente, vendorAllDesarrollo);
+	watch(configuracion.directorios.pug.recargar.dev).on('change', browserSync.reload);
+
+	done();
+}
+// Browser sync prod
+function browserSyncProd(done) {
+	browserSync.init({
+		injectChanges: true,
+		server: {
+			baseDir: "dist/prod",
+			serveStaticOptions: {
+				extensions: ['html']
+			},
+			directory: true
+		},
+		port: 3000
+	});
+
+	let watcherHtml = watch(configuracion.directorios.pug.watcher);
+	watcherHtml.on('change', function (path) { htmlProd(path) });
+	watch(configuracion.directorios.sass.watcher, compilarCssProd);
+	// watch("./js/**/*.js", jsProd);
+	// watch(configuracion.directorios.imagenes.fuente, imagenesProd);
+	// watch(configuracion.directorios.fonts.fuente, fontsProd);
+	// watch(configuracion.directorios.vendor.fuente, vendorAllProd);
+	watch(configuracion.directorios.pug.recargar.dev).on('change', browserSync.reload);
+
+	done();
 }
 
 exports.htmlDesarrollo = htmlDesarrollo;
-exports.compilarCssDesarrollo   = compilarCssDesarrollo;
-exports.jsDesarrollo        = jsDesarrollo;
-exports.imagenesDesarrollo  = imagenesDesarrollo;
-exports.fontsDesarrollo     = fontsDesarrollo;
-exports.vendorAllDesarrollo     = vendorAllDesarrollo;
+exports.htmlProd = htmlProd;
+exports.compilarCssDesarrollo = compilarCssDesarrollo;
+exports.compilarCssProd = compilarCssProd;
 
-const compilarDesarrollo    = series(parallel( compilarCssDesarrollo, jsDesarrollo, htmlDesarrollo, vendorAllDesarrollo, imagenesDesarrollo, fontsDesarrollo ), browserSyncDesarrollo);
+exports.jsDesarrollo = jsDesarrollo;
+exports.jsProd = jsProd;
+exports.imagenesDesarrollo = imagenesDesarrollo;
+exports.imagenesProd = imagenesProd;
+
+exports.fontsDesarrollo = fontsDesarrollo;
+exports.vendorAllDesarrollo = vendorAllDesarrollo;
+
+const compilarDesarrollo = series(parallel(compilarCssDesarrollo, jsDesarrollo, htmlDesarrollo, vendorAllDesarrollo, imagenesDesarrollo, fontsDesarrollo), browserSyncDesarrollo);
+const compilarProduccion = series(parallel(compilarCssProd, htmlProd, jsProd, imagenesProd ), browserSyncProd);
+
 exports.default = compilarDesarrollo;
+exports.produccion = compilarProduccion;
